7b2335aab10a87dbdc062bb0f5a9a0824f880967
akeemkabiru <kabby.dev@outlook.com>
2023-11-22 22:09:01 +0000

    routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
-export default function App() {
-  return (
-    <div>
-      <h1 className="text-blue-900 font-semibold text-4xl">Hello world</h1>
-    </div>
-  );
+export default function Home() {
+  return <div>Home</div>;
 }
