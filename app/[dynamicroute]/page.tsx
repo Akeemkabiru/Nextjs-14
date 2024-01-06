ce44510cb79860eb6e871394578e93202fffe5f8
Kabby <kabby.dev@outlook.com>
2024-01-06 18:54:08 +0000

    metadata-2

--- app/[dynamicroute]/page.tsx
+++ app/[dynamicroute]/page.tsx
@@ -1,10 +1,15 @@
 import Link from "next/link";
-
-export default function Route({
-  params,
-}: {
+import { Metadata } from "next";
+type paramsProps = {
   params: { dynamicroute: string };
-}) {
+};
+
+export const dynamicMetadata = ({ params }: paramsProps) => {
+  title: {
+    `${params.dynamicroute}`;
+  }
+};
+export default function Route({ params }: paramsProps) {
   return (
     <h1>
       Dynamic route created with square bracket {params.dynamicroute}
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "about",
+  description: "This is an about us page",
+};
+
 export default function About() {
   return <h1>About</h1>;
 }
